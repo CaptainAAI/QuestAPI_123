c2669ec96b89a589d16f98b511a9eef7e7af19bb
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-09-02 13:05:10 +0000

    fix PenyediaViewModel

--- app/src/main/java/com/example/phpandroidstudiomysql/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/phpandroidstudiomysql/viewmodel/PenyediaViewModel.kt
@@ -4,14 +4,14 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.phpandroidstudiomysql.repositori.AplikasiDataSiswa
 import com.example.phpandroidstudiomysql.repositori.DefaultContainerApp
 
-fun CreationExtras.aplikasiSiswa(): DefaultContainerApp.AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as DefaultContainerApp.AplikasiDataSiswa)
-object PenyediaViewModel{
+fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositoriSiswa)
-        }
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
     }
 }
